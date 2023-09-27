114eaff8058c9a766a29615f0a73c5dcd14e92d6
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2023-09-27 14:40:24 -0700

    Update to work with latest `epidatr`

--- basic.R
+++ basic.R
@@ -1,27 +1,26 @@
 library(magrittr) # for `%>%`
 
 # Fetch daily HHS hospitalization count data for all states and territories for
-# April 2022 using `epidatr`, a draft new client package for accessing the
-# Delphi Epidata API:
-april = epidatr::covidcast(
+# April 2022 using `epidatr`:
+april = epidatr::pub_covidcast(
   "hhs", "confirmed_admissions_influenza_1d",
   "state", "day",
   geo_values = "*",
   time_values = epidatr::epirange(20220401, 20220430)
-) %>%
-  epidatr::fetch()
+)
+# (You can also use `time_values = "*"` to get data for all times, or
+# `time_values = date_vector` to get data for those dates.)
 
 # Fetch these measurements as they were reported on May 10, rather than the
 # current version:
 april_as_of_may10 =
-  epidatr::covidcast(
+  epidatr::pub_covidcast(
     "hhs", "confirmed_admissions_influenza_1d",
     "state", "day",
     "*",
     epidatr::epirange(20220401, 20220430),
     as_of = 20220510
-  ) %>%
-  epidatr::fetch()
+  )
 
 # Fetch the first data set using the older `covidcast` package:
 april_with_covidcast = covidcast::covidcast_signal(
