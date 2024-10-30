526f586055fc25d179d7db32d59aa7fc5b64c08e
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2024-10-30 09:54:43 -0700

    Remove "now" be cached, "draft" package

--- basic.py
+++ basic.py
@@ -4,12 +4,11 @@ import pandas as pd
 
 from epidatpy import CovidcastEpidata, EpiDataContext, EpiRange
 
-# All calls using the `epidata` object will now be cached for a day
+# All calls using the `epidata` object will be cached for a day
 epidata = EpiDataContext(use_cache=True, cache_max_age_days=1)
 
 # Fetch daily HHS hospitalization count data for all states and territories for
-# April 2022 using `epidatpy`, a draft new client package for accessing the
-# Delphi Epidata API:
+# April 2022:
 april = epidata.pub_covidcast(
     data_source = "hhs",
     signals = "confirmed_admissions_influenza_1d",
